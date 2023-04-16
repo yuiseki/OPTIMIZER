c22c0289c60a4ed7dea44fca031d322f24918992
yuiseki <yuiseki@gmail.com>
2023-04-16 16:35:34 +0900

    refactor css

--- src/components/OPTIMIZER/index.tsx
+++ src/components/OPTIMIZER/index.tsx
@@ -145,20 +145,7 @@ export const OPTIMIZER: React.FC = () => {
         <div style={{ width: "100%", marginBottom: "15em" }}>
           {dialogueList.map((dialogueElement, dialogueIdx) => {
             return (
-              <div
-                key={dialogueIdx}
-                style={{
-                  display: "flex",
-                  flexDirection: "column",
-                  gap: "12px",
-                  marginBottom: "30px",
-                  padding: "12px",
-                  borderRadius: "2px",
-                  border: "2px solid rgba(219, 219, 219, 0.8)",
-                  boxShadow: "0 2px 6px 0 rgba(219, 219, 219, 0.3)",
-                  backgroundColor: "rgba(79, 79, 79, 0.9)",
-                }}
-              >
+              <div className="dialogueElement" key={dialogueIdx}>
                 <div
                   style={{
                     display: "flex",
