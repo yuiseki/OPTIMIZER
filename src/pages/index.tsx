ca8d5a894d773547d01008acbdec5d1565e83a29
yuiseki <yuiseki@gmail.com>
2023-04-16 18:07:25 +0900

    update meta og

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -16,6 +16,19 @@ export default function Home() {
           name="description"
           content="対話型制度探索社会最適化支援システム オプティマイザー 起動しました。 落ち着いて状況を整理し、あなたの抱えている困り事や悩み事を、具体的かつ簡潔に入力してください。"
         />
+        <meta
+          property="og:title"
+          content="対話型制度探索社会最適化支援システム オプティマイザー"
+        />
+        <meta
+          property="og:description"
+          content="対話型制度探索社会最適化支援システム オプティマイザー 起動しました。 落ち着いて状況を整理し、あなたの抱えている困り事や悩み事を、具体的かつ簡潔に入力してください。"
+        />
+        <meta
+          property="og:image"
+          content="https://i.gyazo.com/b1f6cfdd3c3d3ab3d3e877782b450f68.png"
+        />
+        <meta property="og:url" content="https://optimizer.yuiseki.net/" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
