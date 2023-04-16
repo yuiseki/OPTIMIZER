f75808ff78e6209b626e6274358c177a879ab532
yuiseki <yuiseki@gmail.com>
2023-04-16 13:56:08 +0900

    fix mistake of element order

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -19,11 +19,11 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <OPTIMIZER />
-      <SupportModeCard />
-      <CircleClock />
-      <UserInfoCard />
       <BackgroundEffect />
+      <CircleClock />
+      <SupportModeCard />
+      <UserInfoCard />
+      <OPTIMIZER />
     </>
   );
 }
