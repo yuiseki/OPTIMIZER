32cb49546249dff02a27789ab3255e553d85c01e
yuiseki <yuiseki@gmail.com>
2023-04-22 17:16:43 +0900

    add \n\n

--- src/pages/api/completion.ts
+++ src/pages/api/completion.ts
@@ -79,7 +79,7 @@ export default async function handler(
       "Content-Type": "text/event-stream",
       "Transfer-Encoding": "chunked",
     });
-    res.write("ユーザーの入力に従って、社会の最適化を計算しています…");
+    res.write("ユーザーの入力に従って、社会の最適化を計算しています…\n\n");
     const llm = new OpenAI({
       temperature: 0,
       maxTokens: 1000,
